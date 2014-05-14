b14b86e32fbd47d907b488930077b569b754e1d3
Ken MacKay <kmackay@gmail.com>
2014-05-13 22:53:12 -0700

    Update asm generation scripts.

--- scripts/mult_avr.py
+++ scripts/mult_avr.py
@@ -26,66 +26,70 @@ def emit(line, *args):
     print s % args
 
 #### set up registers
-emit("adiw r30, %s" % (size - init_size)) # move z
-emit("adiw r28, %s" % (size - init_size)) # move y
+emit("adiw r30, %s", size - init_size) # move z
+emit("adiw r28, %s", size - init_size) # move y
 
 for i in xrange(init_size):
     emit("ld r%s, x+", rx(i))
 for i in xrange(init_size):
     emit("ld r%s, y+", ry(i))
 
-# note that this code assume that init_size > 1
-#### first two multiplications of initial block
 emit("ldi r25, 0")
 print ""
-emit("ldi r23, 0")
-emit("mul r2, r12")
-emit("st z+, r0")
-emit("mov r22, r1")
-print ""
-emit("ldi r24, 0")
-emit("mul r2, r13")
-emit("add r22, r0")
-emit("adc r23, r1")
-emit("mul r3, r12")
-emit("add r22, r0")
-emit("adc r23, r1")
-emit("adc r24, r25")
-emit("st z+, r22")
-print ""
+if init_size == 1:
+    emit("mul r2, r12")
+    emit("st z+, r0")
+    emit("st z+, r1")
+else:
+    #### first two multiplications of initial block
+    emit("ldi r23, 0")
+    emit("mul r2, r12")
+    emit("st z+, r0")
+    emit("mov r22, r1")
+    print ""
+    emit("ldi r24, 0")
+    emit("mul r2, r13")
+    emit("add r22, r0")
+    emit("adc r23, r1")
+    emit("mul r3, r12")
+    emit("add r22, r0")
+    emit("adc r23, r1")
+    emit("adc r24, r25")
+    emit("st z+, r22")
+    print ""
 
-#### rest of initial block, with moving accumulator registers
-acc = [23, 24, 22]
-for r in xrange(2, init_size):
-    emit("ldi r%s, 0", acc[2])
-    for i in xrange(0, r+1):
-        emit("mul r%s, r%s", rx(i), ry(r - i))
-        emit("add r%s, r0", acc[0])
-        emit("adc r%s, r1", acc[1])
-        emit("adc r%s, r25", acc[2])
+    #### rest of initial block, with moving accumulator registers
+    acc = [23, 24, 22]
+    for r in xrange(2, init_size):
+        emit("ldi r%s, 0", acc[2])
+        for i in xrange(0, r+1):
+            emit("mul r%s, r%s", rx(i), ry(r - i))
+            emit("add r%s, r0", acc[0])
+            emit("adc r%s, r1", acc[1])
+            emit("adc r%s, r25", acc[2])
+        emit("st z+, r%s", acc[0])
+        print ""
+        acc = acc[1:] + acc[:1]
+    for r in xrange(1, init_size-1):
+        emit("ldi r%s, 0", acc[2])
+        for i in xrange(0, init_size-r):
+            emit("mul r%s, r%s", rx(r+i), ry((init_size-1) - i))
+            emit("add r%s, r0", acc[0])
+            emit("adc r%s, r1", acc[1])
+            emit("adc r%s, r25", acc[2])
+        emit("st z+, r%s", acc[0])
+        print ""
+        acc = acc[1:] + acc[:1]
+    emit("mul r%s, r%s", rx(init_size-1), ry(init_size-1))
+    emit("add r%s, r0", acc[0])
+    emit("adc r%s, r1", acc[1])
     emit("st z+, r%s", acc[0])
-    print ""
-    acc = acc[1:] + acc[:1]
-for r in xrange(1, init_size-1):
-    emit("ldi r%s, 0", acc[2])
-    for i in xrange(0, init_size-r):
-        emit("mul r%s, r%s", rx(r+i), ry((init_size-1) - i))
-        emit("add r%s, r0", acc[0])
-        emit("adc r%s, r1", acc[1])
-        emit("adc r%s, r25", acc[2])
-    emit("st z+, r%s", acc[0])
-    print ""
-    acc = acc[1:] + acc[:1]
-emit("mul r%s, r%s", rx(init_size-1), ry(init_size-1))
-emit("add r%s, r0", acc[0])
-emit("adc r%s, r1", acc[1])
-emit("st z+, r%s", acc[0])
-emit("st z+, r%s", acc[1])
+    emit("st z+, r%s", acc[1])
 print ""
 
 #### reset y and z pointers
-emit("sbiw r30, %s" % (2 * init_size + 10))
-emit("sbiw r28, %s" % (init_size + 10))
+emit("sbiw r30, %s", 2 * init_size + 10)
+emit("sbiw r28, %s", init_size + 10)
 
 #### load y registers
 for i in xrange(10):
@@ -186,9 +190,9 @@ for row in xrange(full_rows):
     prev_size = prev_size + 10
     if row < full_rows - 1:
         #### reset x, y and z pointers
-        emit("sbiw r30, %s" % (2 * prev_size + 10))
-        emit("sbiw r28, %s" % (prev_size + 10))
-        emit("sbiw r26, %s" % (prev_size))
+        emit("sbiw r30, %s", 2 * prev_size + 10)
+        emit("sbiw r28, %s", prev_size + 10)
+        emit("sbiw r26, %s", prev_size)
 
         #### load x and y registers
         for i in xrange(10):
