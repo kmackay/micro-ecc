4014bbd37856176e553d6e99a7e2de590e1a7dec
Ken MacKay <kmackay@gmail.com>
2015-09-25 21:07:49 -0700

    Fix formatting.

--- uECC.c
+++ uECC.c
@@ -860,14 +860,11 @@ uECC_VLI_API int uECC_generate_random_int(uECC_word_t *random,
     for (tries = 0; tries < uECC_RNG_MAX_TRIES; ++tries) {
         if (!g_rng_function((uint8_t *)random, num_words * uECC_WORD_SIZE)) {
             return 0;
-	}
-	else {
-            random[num_words - 1] &= mask >> ((bitcount_t)(num_words * uECC_WORD_SIZE * 8 - num_bits));
-
-            if (!uECC_vli_isZero(random, num_words) &&
-		    uECC_vli_cmp(top, random, num_words) == 1) {
-                return 1;
-            }
+	    }
+        random[num_words - 1] &= mask >> ((bitcount_t)(num_words * uECC_WORD_SIZE * 8 - num_bits));
+        if (!uECC_vli_isZero(random, num_words) &&
+		        uECC_vli_cmp(top, random, num_words) == 1) {
+            return 1;
         }
     }
     return 0;
