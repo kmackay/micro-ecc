ceae2ca0883291536ede3b5aa1f4ea852233f3d7
cacu <carles.cufi@nordicsemi.no>
2016-03-08 09:49:04 +0100

    Fix curly brace placement

--- uECC.c
+++ uECC.c
@@ -159,8 +159,7 @@ struct uECC_Curve_t {
 
 static void bcopy(uint8_t *dst,
                   const uint8_t *src,
-                  unsigned num_bytes)
-{
+                  unsigned num_bytes) {
     while (0 != num_bytes) {
         num_bytes--;
         dst[num_bytes] = src[num_bytes];
