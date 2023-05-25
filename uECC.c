8e9642e9627dfa38b7c9620ed5df83cf958af0a4
Leonard Chan <leonardchan@google.com>
2023-05-25 23:24:34 +0000

    Never inline uECC_vli_add/sub/clear.
    Clang will unconditionally inline these large assembly functions at -Oz.
    This should be addressed upstream, but in the meantime, we can save some
    kilobytes by forcing them to never be inlined.

--- uECC.c
+++ uECC.c
@@ -209,6 +209,7 @@ int uECC_curve_public_key_size(uECC_Curve curve) {
 }
 
 #if !asm_clear
+__attribute__((noinline))
 uECC_VLI_API void uECC_vli_clear(uECC_word_t *vli, wordcount_t num_words) {
     wordcount_t i;
     for (i = 0; i < num_words; ++i) {
